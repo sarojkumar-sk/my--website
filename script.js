e6d4ddc40db279f14b332787808a228c6130f3e7
Saroj Kumar <saroj1011535@gmail.com>
2026-04-27 09:03:13 +0530

    Implement GitHub projects loading in script.js
    Added GitHub projects loading functionality.

--- script.js
+++ script.js
@@ -25,3 +25,30 @@ const navLinks = document.getElementById("navLinks");
 menuBtn.onclick = function () {
     navLinks.classList.toggle("show");
 };
+async function loadGitHubProjects() {
+    let container = document.getElementById("github-projects");
+
+    try {
+        let response = await fetch("https://api.github.com/users/sarojkumar-sk/repos");
+        let data = await response.json();
+
+        container.innerHTML = "";
+
+        data.forEach(repo => {
+            let div = document.createElement("div");
+            div.className = "project-card";
+
+            div.innerHTML = `
+                <h3>${repo.name}</h3>
+                <a href="${repo.html_url}" target="_blank">View Code</a>
+            `;
+
+            container.appendChild(div);
+        });
+
+    } catch (error) {
+        container.innerHTML = "Error loading projects";
+    }
+}
+
+loadGitHubProjects();
